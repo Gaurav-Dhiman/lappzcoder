2e20fa64b98a51e37685ba79fbcb8dc636561b0a
codeg8 <mohany2712@gmail.com>
2017-12-08 22:05:50 +0530

    Added 404 Page

---
+++ resources/views/front-end/error/404.blade.php
@@ -0,0 +1,10 @@
+@extends('front-end.layout.main')
+
+
+@section('main-content')
+    <div class="container-fluid">
+        <div class="row">
+            <img src="{{ asset('front-end-assets/images/error.jpg') }}" style="width:100%">
+        </div>
+    </div>
+@endsection
